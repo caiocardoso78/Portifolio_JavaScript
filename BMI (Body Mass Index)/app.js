9d210a13799e9ab78688ca99cc06f34dbf25fabb
Caio Cesar Camargo Cardoso <caiocardoso78@gmail.com>
2022-11-12 14:45:21 -0300

    feat: Using toFixed to set decimal places

--- BMI (Body Mass Index)/app.js
+++ BMI (Body Mass Index)/app.js
@@ -1,12 +1,25 @@
-const nome = document.getElementById('nome');
-const altura = document.getElementById('altura');
-const peso = document.getElementById('peso');
+
 const calcular = document.getElementById('calcular');
-const result = document.getElementById('result');
+
 
 function imc () {
-  
+    const nome = document.getElementById('nome').value;
+    const altura = document.getElementById('altura').value;
+    const peso = document.getElementById('peso').value;
+    const resultado = document.getElementById('resultado');
+
+    if (nome !== '' && altura !== '' && peso !== '') {
+       
+        const valorIMC = (peso / (altura*altura)).toFixed(1);
+        
+        resultado.textContent = valorIMC;
+
+    }else {
+        resultado.textContent = 'Preencha todos os campos!'
+    }
+
 }
+
 calcular.addEventListener('click', imc);
 
 
