518a3cde8ff23b120a7585a31a0d91dd1bc26fb8
Caio Cesar Camargo Cardoso <caiocardoso78@gmail.com>
2022-11-12 14:13:18 -0300

    feat: creating function 'imc'

---
+++ BMI (Body Mass Index)/app.js
@@ -0,0 +1,12 @@
+const nome = document.getElementById('nome');
+const altura = document.getElementById('altura');
+const peso = document.getElementById('peso');
+const calcular = document.getElementById('calcular');
+const result = document.getElementById('result');
+
+function imc () {
+  
+}
+calcular.addEventListener('click', imc);
+
+
